8afd1a10b9aa7eaf38b9e653116a42f8534bf5c1
zhangte007 <zt2625343872@qq.com>
2018-12-09 22:15:00 +0800

    初次提交。

---
+++ src/main/java/com/zhangte/springcloud/demo/SpringCloudConfigServerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zhangte.springcloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerDemoApplication.class, args);
+	}
+}
